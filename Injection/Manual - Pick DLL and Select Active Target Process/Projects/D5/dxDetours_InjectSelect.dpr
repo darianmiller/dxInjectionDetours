9fb030b683f86bbe81e9904534edbb1ac9e48971
Darian Miller <darian@darianmiller.com>
2019-11-06 15:33:23 -0600

    Unit name correction

--- Injection/Manual - Pick DLL and Select Active Target Process/Projects/D5/dxDetours_InjectSelect.dpr
+++ Injection/Manual - Pick DLL and Select Active Target Process/Projects/D5/dxDetours_InjectSelect.dpr
@@ -4,11 +4,11 @@ uses
   Forms,
   dxDetours_InjectSelect_MainForm in '..\..\Source\dxDetours_InjectSelect_MainForm.pas' {frmMain},
   dxLib_ProcessList in '..\..\..\..\Dependencies\dxLib\Source\dxLib_ProcessList.pas',
-  dxLib_ProcessNameToId in '..\..\..\..\Dependencies\dxLib\Source\dxLib_ProcessNameToId.pas',
   dxLib_Types in '..\..\..\..\Dependencies\dxLib\Source\dxLib_Types.pas',
   dxLib_WinApi in '..\..\..\..\Dependencies\dxLib\Source\dxLib_WinApi.pas',
   dxLib_WinInjection in '..\..\..\..\Dependencies\dxLib\Source\dxLib_WinInjection.pas',
-  dxLib_Strings in '..\..\..\..\Dependencies\dxLib\Source\dxLib_Strings.pas';
+  dxLib_Strings in '..\..\..\..\Dependencies\dxLib\Source\dxLib_Strings.pas',
+  dxLib_ProcessFileNameFromId in '..\..\..\..\Dependencies\dxLib\Source\dxLib_ProcessFileNameFromId.pas';
 
 {$R *.RES}
 
